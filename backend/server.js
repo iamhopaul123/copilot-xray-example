d924b7a84e32b60f40b1a5e52249a31a949dc4a3
penghaoh <penghaoh@amazon.com>
2021-10-18 16:25:54 -0700

    Add segment setter

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,38 @@
-'use strict';
+"use strict";
 
 console.log(`region is: ${process.env.AWS_DEFAULT_REGION}`);
 
-const AWSXRay = require('aws-xray-sdk');
-const AWS = require('aws-sdk');
+const xray = require("aws-xray-sdk");
+const aws = require("aws-sdk");
 
-const AWSSdk = AWSXRay.captureAWS(AWS);
-AWSSdk.config.update({region: process.env.AWS_DEFAULT_REGION});
-const sns = new AWSSdk.SNS();
+const modified = xray.captureAWS(aws);
+modified.config.update({ region: process.env.AWS_DEFAULT_REGION });
+const sns = new modified.SNS();
 
 function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
+const segment = new xray.Segment("job-runner");
+const ns = xray.getNamespace();
+ns.run(async () => {
+  const { loadtest } = JSON.parse(process.env.COPILOT_SNS_TOPIC_ARNS);
+  while (true) {
+    console.log('setSegment');
+    xray.setSegment(segment);
+    try {
+      const out = await sns
+        .publish({
+          Message: "test",
+          TopicArn: loadtest,
+        })
+        .promise();
 
-(async () => {
-  const {loadtest} = JSON.parse(process.env.COPILOT_SNS_TOPIC_ARNS);
-  while(true) {
-      try {
-          const out = await sns.publish({
-            Message: 'test',
-            TopicArn: loadtest,
-          }).promise();
-  
-          console.log(`results: ${JSON.stringify(out)}`);
-      } catch (err) {
-          console.error(err);
-      }
-      await sleep(50);
+      console.log(`results: ${JSON.stringify(out)}`);
+    } catch (err) {
+      console.error(err);
+    }
+    segment.close();
+    await sleep(50);
   }
-})();
+});
